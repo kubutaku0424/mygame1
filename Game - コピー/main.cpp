c4edce666b9268a5513b6e2ea8f590fe0b0c77e6
kubutaku0424 <takuyuu0839@outlook.jp>
2021-04-14 15:50:31 +0900

    Gitにゲームフォルダを追加

---
+++ Game - コピー/main.cpp
@@ -0,0 +1,21 @@
+
+#include "DxLib.h"
+#include "SceneMgr.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
+
+    SceneMgr_Initialize();
+
+    while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {//画面更新 & メッセージ処理 & 画面消去
+
+        SceneMgr_Update();  //更新
+        SceneMgr_Draw();    //描画
+
+    }
+
+    SceneMgr_Finalize();
+
+    DxLib_End(); // DXライブラリ終了処理
+    return 0;
+}
